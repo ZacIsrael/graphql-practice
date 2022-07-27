1f1cad608531d49afae9b954df4659ba3fdb12a3
ZacIsrael <zac390@gmail.com>
2022-07-27 03:30:10 -0400

    modified buildSchema in server.js, added more notes in txt file

--- server.js
+++ server.js
@@ -10,16 +10,90 @@ const { graphqlHTTP } = require('express-graphql');
 
 
 // buildig schema for an ecommerce API
+// the type Query lists all of the different pieces of data that can be queried for in the API
+// the explanation mark after the type indicates that the field is required 
 const schema = buildSchema(`
     type Query {
-        description: String
-        price: Float
+        products: [Product]
+        orders: [Order]
+    }
+
+    type Product {
+        id: ID!
+        description: String!
+        reviews: [Review]
+        price: Float!
+    }
+
+    type Review {
+        rating: Int!
+        comment: String
+    }
+
+    type Order {
+        date: String!
+        subtotal: Float!
+        items: [OrderItem]
+    }
+
+    type OrderItem {
+        product: Product!
+        quantity: Int!
     }
 `);
 
 const root = {
-    description: 'Red Shoe',
-    price: 42.12
+    products: [
+        {
+            id: 'redshoe',
+            description: 'Red Shoe',
+            price: 42.12
+        },
+        {
+            id: 'blueshoe',
+            description: 'Blue Shoe',
+            price: 102.35
+        },
+        {
+            id: 'greensirt',
+            description: 'Green Polo Shirt',
+            price: 50.00
+        },
+        {
+            id: 'blackpants',
+            description: 'Black Cargo Pants',
+            price: 60.00
+        },
+        {
+            id: 'whiteshirt',
+            description: 'White T-shirt',
+            price: 10.00
+        },
+    ],
+    orders: [
+        {
+            date: '2020-07-02',
+            subtotal: 130.00,
+            items: [
+                {
+                    product: {
+                        id: 'blackpants',
+                        description: 'Black Cargo Pants',
+                        price: 60.00
+                    },
+                    quantity: 2
+                },
+                {
+                    product: {
+                        id: 'whiteshirt',
+                        description: 'White T-shirt',
+                        price: 10.00
+                    },
+                    quantity: 1
+                }
+            ]
+        }
+    ]
 }
 // application object
 const app = express();
